56a30b21f53431e7b8801eced74708dafa67eefa
Raghav Mittal <raghav_mittal@sfu.ca>
2019-10-15 14:22:58 -0700

    Add analyse data file for e6

--- Exercises/e6/analyse_data.py
+++ Exercises/e6/analyse_data.py
@@ -1,5 +1,22 @@
 import numpy as np
 import pandas as pd
+from scipy import stats
+from implementations import all_implementations
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+from matplotlib import pyplot as plt 
 
 df = pd.read_csv('data.csv')
-print(df)
+rankings = []
+
+test = stats.f_oneway(df.qs1, df.qs2, df.qs3, df.qs4, df.qs5, df.merge1, df.partition_sort)
+
+df = df.melt()
+# print(df)
+fig,ax = plt.subplots()
+posthoc = pairwise_tukeyhsd(
+    df['value'], df['variable'],
+    alpha=0.05)
+
+posthoc.plot_simultaneous(ax=ax)
+
+
