598ce5ebd91779549f2327b21d2b6c54dade4124
Raghav Mittal <raghav_mittal@sfu.ca>
2019-11-26 08:35:39 -0800

    fix sorting

--- Exercises/e12/wordcount.py
+++ Exercises/e12/wordcount.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession, functions, types, Row
 import re
 import numpy as np
 import pandas as pd
-import string, re
+import string
 
 spark = SparkSession.builder.appName('word count').getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
@@ -12,23 +12,16 @@ assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 assert spark.version >= '2.3' # make sure we have Spark 2.3+
 
 wordbreak = r'[%s\s]+' % (re.escape(string.punctuation),)  # regex that matches spaces and/or punctuation
-compiled_re = re.compile(wordbreak)
-
-def line_to_row(line):
-    """
-    Take a logfile line and return a Row object with hostname and bytes transferred. Return None if regex doesn't match.
-    """
-    return re.split(compiled_re, line)
 
 def main(in_directory, out_directory):
-    lines =  spark.read.text(in_directory)
-    df = lines.select(functions.explode(functions.split(lines['value'], wordbreak)).alias('word'))
-    df = df.select(functions.lower(df['word']).alias('word'))
-    df = df.groupBy(df.word).agg(functions.count(functions.lit(1)).alias('count'))
-    df = df.orderBy(['count', 'word'], ascending=[0, 1])
-    df = df.filter(df['word'] != '')
-    df.show()
-    df.write.csv(out_directory, mode='overwrite')
+    words = spark.read.text(in_directory)
+    words = words.select(functions.explode(functions.split(words['value'], wordbreak)).alias('word'))
+    words = words.select(functions.lower(words['word']).alias('word'))
+    words = words.groupBy(words.word).agg(functions.count(functions.lit(1)).alias('count'))
+    words = words.orderBy(['count', 'word'], ascending=[0, 1])
+    words = words.filter(words['word'] != '')
+    words.show()
+    words.write.csv(out_directory, mode='overwrite')
 
 
 if __name__=='__main__':
